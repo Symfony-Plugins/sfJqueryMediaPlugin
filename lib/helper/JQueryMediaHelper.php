a688a7fb57b878298db677990a45ac0afbb872f9
Yoda-BZH <Yoda-BZH@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-07-17 14:41:49 -0500

    * it can now handle player change * code comments * options are sorted
    git-svn-id: http://svn.symfony-project.com/plugins/sfJqueryMediaPlugin/trunk@20260 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/helper/JQueryMediaHelper.php
+++ lib/helper/JQueryMediaHelper.php
@@ -19,13 +19,42 @@
 
 
 
-
+/**
+ * The jQuery Media Plugin supports unobtrusive conversion of standard markup into rich media content.
+ * It can be used to embed virtually any media type, including Flash, Quicktime, Windows Media Player, Real Player,
+ * MP3, Silverlight, PDF and more, into a web page. The plugin converts an element (usually an <a>)
+ * into a <div> which holds the object, embed or iframe tags neccessary to render the media content.
+ *
+ * @author Yoda-BZH, yodabzh@gmail.com
+ * @param $pId the id of the link
+ * @param $pOptions an array of options
+ * @param $pHtmlAttributes unused
+ * @return javascript code
+ */
 function jq_media($pId, array $pOptions = array(), array $pHtmlAttributes = array()) {
+    /**
+     * adding javascript files to the headers
+     */
     $response = sfContext::getInstance()->getResponse();
     $response->addJavascript('/sfJqueryMediaPlugin/js/jquery.media.js');
+    /**
+     * apparently jquery media doesn't support on-the-fly by-option player change ...
+     */
+    $preferences = '';
+    foreach(array('mp3Player', 'flvPlayer') as $player) {
+        if(array_key_exists($player, $pOptions)) {
+            $preferences .= '    $.fn.media.defaults.'.$player.' = "'.$pOptions[$player].'"'."\n";
+            unset($pOptions[$player]);
+        }
+    }
+    /**
+     * sort the options by *key* name
+     */
+    ksort($pOptions);
     $opts = jq_media_compute_options($pOptions);
     $code = '
 jQuery(document).ready(function() {
+'.$preferences.'
     jQuery("#'.$pId.'").media({
         '.$opts.'
     });
@@ -34,16 +63,18 @@ jQuery(document).ready(function() {
     return javascript_tag($code);
 }
 
-
+/**
+ * return a string for a well formated json array
+ * @param $pOptions
+ * @return a string with the options
+ * @author Yoda-BZH, yodabzh@gmail.com
+ */
 function jq_media_compute_options($pOptions) {
     /**
      * compiling js options
      */
     $opts = array();
     foreach($pOptions as $k => $v) {
-        //if($k == 'height' || $k == 'width') {
-        //    $v .= 'px';
-        //}
         switch(gettype($v)) {
             case 'int' :
             case 'integer' :
